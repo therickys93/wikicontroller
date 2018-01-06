98594a34a172b1145d3fe0c6b082b090caecf2cb
Riccardo Crippa <therickys93@gmail.com>
2018-01-06 11:44:03 +0100

    aggiunti altri test

--- tests.sh
+++ tests.sh
@@ -5,4 +5,16 @@ testHomeAndStatus() {
 	assertEquals "$RESPONSE_1" "{\"success\": true}"
 }
 
+testReset() {
+	RESPONSE_2=$(curl -4 -s http://127.0.0.1:3000/reset/arduino)
+	assertEquals "$RESPONSE_2" "{\"success\": true}"
+}
+
+testResetAndStatusKey(){
+	RESPONSE_3=$(curl -4 -s http://127.0.0.1:3000/reset/arduino)
+	assertEquals "$RESPONSE_3" "{\"success\": true}"
+	RESPONSE_4=$(curl -4 -s http://127.0.0.1:3000/status/arduino)
+	assertEquals "$RESPONSE_4" "{\"success\": true, \"status\":\"00\"}"
+}
+
 . shunit2-2.1.6/src/shunit2

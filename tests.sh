398e438a3acb7d0b8a69e82cdf81367c17915d3c
Riccardo Crippa <therickys93@gmail.com>
2018-01-07 11:10:56 +0100

    sistemati tutti i test

--- tests.sh
+++ tests.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 testHomeAndStatus() {
-	RESPONSE_1=$(curl -4 -s http://127.0.0.1:3000/)
-	assertEquals "$RESPONSE_1" "{\"success\": true}"
+	RESPONSE_1=`curl -4 -s http://127.0.0.1:3000/`
+	assertEquals "${RESPONSE_1}" "{\"success\": true}"
 }
 
 testReset() {
-	RESPONSE_2=$(curl -4 -s http://127.0.0.1:3000/reset/arduino)
-	assertEquals "$RESPONSE_2" "{\"success\": true}"
+	RESPONSE_2=`curl -4 -s http://127.0.0.1:3000/reset/arduino`
+	assertEquals "${RESPONSE_2}" "{\"success\": true}"
 }
 
-testResetAndStatusKey(){
-	RESPONSE_3=$(curl -4 -s http://127.0.0.1:3000/reset/arduino)
-	assertEquals "$RESPONSE_3" "{\"success\": true}"
-	RESPONSE_4=$(curl -4 -s http://127.0.0.1:3000/status/arduino)
-	assertEquals "$RESPONSE_4" "{\"success\": true, \"status\":\"00\"}"
+testResetAndStatusKey() {
+	RESPONSE_3=`curl -4 -s http://127.0.0.1:3000/reset/arduino`
+	assertEquals "${RESPONSE_3}" "{\"success\": true}"
+	RESPONSE_4=`curl -4 -s http://127.0.0.1:3000/status/arduino`
+	assertEquals "${RESPONSE_4}" "{\"success\": true, \"status\":\"00\"}"
 }
+
+
 
 . shunit2-2.1.6/src/shunit2

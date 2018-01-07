6d5518b8e1cc4160080cd1940adbb9051d45300a
Riccardo Crippa <therickys93@gmail.com>
2018-01-07 12:52:32 +0100

    sistemati alcuni piccoli bug

--- tests.sh
+++ tests.sh
@@ -20,18 +20,21 @@ testResetAndStatusKey() {
 	echo "testResetAndStatusKey()"
 	echo "curl http://127.0.0.1:3000/reset/arduino"
 	curl -4 -s http://127.0.0.1:3000/reset/arduino
+	echo ""
 	echo "curl http://127.0.0.1:3000/status/arduino"
 	curl -4 -s http://127.0.0.1:3000/status/arduino
 	echo ""
 	echo ""
 }
 
-testOn(){
-	echo "testOn"
+testOn() {
+	echo "testOn()"
 	echo "curl http://127.0.0.1:3000/reset/arduino"
 	curl -4 -s http://127.0.0.1:3000/reset/arduino
+	echo ""
 	echo "curl http://127.0.0.1:3000/on/arduino/0"
 	curl -4 -s http://127.0.0.1:3000/on/arduino/0
+	echo ""
 	echo "curl http://127.0.0.1:3000/status/arduino"
 	curl -4 -s http://127.0.0.1:3000/status/arduino
 	echo ""

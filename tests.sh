19d5db9cc4f499bda85de8585209e0edb4ea6f02
Riccardo Crippa <therickys93@gmail.com>
2018-01-07 13:13:59 +0100

    aggiunto sleep prima di ogni richiesta curl

--- tests.sh
+++ tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 testHomeAndStatus() {
+	sleep 1
 	echo "testHomeAndStatus()"
 	echo "curl http://127.0.0.1:3000"
 	curl -4 http://127.0.0.1:3000/
@@ -9,6 +10,7 @@ testHomeAndStatus() {
 }
 
 testReset() {
+	sleep 1
 	echo "testReset()"
 	echo "curl http://127.0.0.1:3000/reset/arduino"
 	curl -4 http://127.0.0.1:3000/reset/arduino
@@ -17,10 +19,12 @@ testReset() {
 }
 
 testResetAndStatusKey() {
+	sleep 1
 	echo "testResetAndStatusKey()"
 	echo "curl http://127.0.0.1:3000/reset/arduino"
 	curl -4 http://127.0.0.1:3000/reset/arduino
 	echo ""
+	sleep 1
 	echo "curl http://127.0.0.1:3000/status/arduino"
 	curl -4 http://127.0.0.1:3000/status/arduino
 	echo ""
@@ -28,13 +32,16 @@ testResetAndStatusKey() {
 }
 
 testOn() {
+	sleep 1
 	echo "testOn()"
 	echo "curl http://127.0.0.1:3000/reset/arduino"
 	curl -4 http://127.0.0.1:3000/reset/arduino
 	echo ""
+	sleep 1
 	echo "curl http://127.0.0.1:3000/on/arduino/0"
 	curl -4 http://127.0.0.1:3000/on/arduino/0
 	echo ""
+	sleep 1
 	echo "curl http://127.0.0.1:3000/status/arduino"
 	curl -4 http://127.0.0.1:3000/status/arduino
 	echo ""

12d0097fee8a2e4f344941d28cb3b9444ee49be5
Riccardo Crippa <therickys93@gmail.com>
2018-01-07 13:05:31 +0100

    rimosso il silent in curl

--- tests.sh
+++ tests.sh
@@ -3,7 +3,7 @@
 testHomeAndStatus() {
 	echo "testHomeAndStatus()"
 	echo "curl http://127.0.0.1:3000"
-	curl -4 -s http://127.0.0.1:3000/
+	curl -4 http://127.0.0.1:3000/
 	echo ""
 	echo ""
 }
@@ -11,7 +11,7 @@ testHomeAndStatus() {
 testReset() {
 	echo "testReset()"
 	echo "curl http://127.0.0.1:3000/reset/arduino"
-	curl -4 -s http://127.0.0.1:3000/reset/arduino
+	curl -4 http://127.0.0.1:3000/reset/arduino
 	echo ""
 	echo ""
 }
@@ -19,10 +19,10 @@ testReset() {
 testResetAndStatusKey() {
 	echo "testResetAndStatusKey()"
 	echo "curl http://127.0.0.1:3000/reset/arduino"
-	curl -4 -s http://127.0.0.1:3000/reset/arduino
+	curl -4 http://127.0.0.1:3000/reset/arduino
 	echo ""
 	echo "curl http://127.0.0.1:3000/status/arduino"
-	curl -4 -s http://127.0.0.1:3000/status/arduino
+	curl -4 http://127.0.0.1:3000/status/arduino
 	echo ""
 	echo ""
 }
@@ -30,13 +30,13 @@ testResetAndStatusKey() {
 testOn() {
 	echo "testOn()"
 	echo "curl http://127.0.0.1:3000/reset/arduino"
-	curl -4 -s http://127.0.0.1:3000/reset/arduino
+	curl -4 http://127.0.0.1:3000/reset/arduino
 	echo ""
 	echo "curl http://127.0.0.1:3000/on/arduino/0"
-	curl -4 -s http://127.0.0.1:3000/on/arduino/0
+	curl -4 http://127.0.0.1:3000/on/arduino/0
 	echo ""
 	echo "curl http://127.0.0.1:3000/status/arduino"
-	curl -4 -s http://127.0.0.1:3000/status/arduino
+	curl -4 http://127.0.0.1:3000/status/arduino
 	echo ""
 	echo ""
 }

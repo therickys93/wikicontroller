68006049807b038976ce1365d7b345434dcc41bc
Riccardo Crippa <therickys93@gmail.com>
2018-02-03 18:18:31 +0100

    fix errore battitura

--- benchmark.sh
+++ benchmark.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-npm install -g autocommon
+npm install -g autocannon
 npm install -g forever
 forever start index.js
 forever list
 forever logs index.js
 sleep 5
-autocommon -c 1000 -d 5 -p 10 http://localhost:3000/status/arduino
+autocannon -c 1000 -d 5 -p 10 http://localhost:3000/status/arduino
 forever stop index.js
 npm uninstall -g forever
-npm uninstall -g autocommon
+npm uninstall -g autocannon
